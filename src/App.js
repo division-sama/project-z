6e361d42e2867bab79132adec021681a97511493
division-sama <98158934+division-sama@users.noreply.github.com>
2023-09-21 21:48:16 +0530

    trial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   
   return (
     <div className="App">
-      <Header></Header>
+      {/* <Header></Header> */}
       {auth ? <BusinessPage></BusinessPage> : <Example></Example>}
     </div>
   );
